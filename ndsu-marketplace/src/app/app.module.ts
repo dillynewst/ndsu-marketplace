2279eb85fc4804b629c158586b53133867521db6
dillynewst <78389635+dillynewst@users.noreply.github.com>
2021-05-06 18:21:57 -0500

    added sell component

--- ndsu-marketplace/src/app/app.module.ts
+++ ndsu-marketplace/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BuyComponent } from './buy/buy.component';
+import { SellComponent } from './sell/sell.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuyComponent
+    BuyComponent,
+    SellComponent
   ],
   imports: [
     BrowserModule
