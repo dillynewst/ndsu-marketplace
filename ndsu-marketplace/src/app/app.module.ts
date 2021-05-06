6b6398706278d8d107fafa4e649a1bea6fa4134e
dillynewst <78389635+dillynewst@users.noreply.github.com>
2021-05-06 18:23:46 -0500

    added items component

--- ndsu-marketplace/src/app/app.module.ts
+++ ndsu-marketplace/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { BuyComponent } from './buy/buy.component';
 import { SellComponent } from './sell/sell.component';
 import { SearchComponent } from './search/search.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuyComponent,
     SellComponent,
-    SearchComponent
+    SearchComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
