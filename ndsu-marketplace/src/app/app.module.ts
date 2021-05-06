394bd9a923b520aefa636efc9aaf681608067d1d
dillynewst <78389635+dillynewst@users.noreply.github.com>
2021-05-06 18:23:22 -0500

    added search component

--- ndsu-marketplace/src/app/app.module.ts
+++ ndsu-marketplace/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BuyComponent } from './buy/buy.component';
 import { SellComponent } from './sell/sell.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BuyComponent,
-    SellComponent
+    SellComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
