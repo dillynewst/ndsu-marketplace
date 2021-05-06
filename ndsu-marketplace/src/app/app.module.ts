ec9042a4f746512f962cfbd6d32946ef512d82db
dillynewst <78389635+dillynewst@users.noreply.github.com>
2021-05-06 18:20:57 -0500

    added buy component

--- ndsu-marketplace/src/app/app.module.ts
+++ ndsu-marketplace/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BuyComponent } from './buy/buy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyComponent
   ],
   imports: [
     BrowserModule
