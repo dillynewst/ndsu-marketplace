80e92763275adce4d2a9ec5facdedf0cf1d0089c
dillynewst <78389635+dillynewst@users.noreply.github.com>
2021-05-06 19:25:29 -0500

    updated item service

--- ndsu-marketplace/src/app/item.service.ts
+++ ndsu-marketplace/src/app/item.service.ts
@@ -4,22 +4,45 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ItemService {
+  // Types of items : text_books, furniture, clothes, electronics, school_materials, etc
   items = [
     {
-      // test items
-    }
+      itemName : 'Television',
+      itemType : 'electronics',
+      itemDescription : '55 inch Samsung',
+      itemPrice : 120.00,
+      itemSeller : 'john.doe@ndsu.edu'
+    },
+    {
+      itemName : 'Couch',
+      itemType : 'furniture',
+      itemDescription : 'black leather loveseat',
+      itemPrice : 120.00,
+      itemSeller : 'jane.doe@ndsu.edu'
+    },
   ]
 
-  addItem(){
-    this.items.push();
+  addItem(itemName: string, itemType: string, itemDescription: string, itemPrice: number, itemSeller: string){
+    this.items.push({itemName, itemType, itemDescription, itemPrice, itemSeller});
   }
 
-  editItem(){
-
+  editItem(id: number, itemName: string, itemType: string, itemDescription: string, itemPrice: number, itemSeller: string){
+    this.items[id].itemName = itemName;
+    this.items[id].itemType = itemType;
+    this.items[id].itemDescription = itemDescription;
+    this.items[id].itemPrice = itemPrice;
+    this.items[id].itemSeller = itemSeller;
   }
 
-  removeItem(){
-    
+  removeItem(index: number){
+    if(index > -1){
+      this.items.splice(index, 1);
+    }
   }
+
+  getItem(index: number){
+    return this.items[index];
+  }
+  
   constructor() { }
 }
